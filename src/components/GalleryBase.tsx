0fa16b25ddda3456031817c2b32a827fbe9c06ca
Aitor Blesa <aitor.ble@gmail.com>
2024-02-14 09:13:57 +0100

    test

---
+++ src/components/GalleryBase.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "preact/hooks";
+// import Gallery from "./Gallery";
+
+export default function GalleryBase() {
+
+  const [location, setLocation] = useState<string | null>('')
+
+  useEffect(() => {
+    const init = async () => {
+
+      const module = await import('photoswipe/lightbox');
+      const PhotoSwipeLightbox = module.default;
+      const lightbox = new PhotoSwipeLightbox({
+        gallery: '#gallery',
+        children: 'a',
+        pswpModule: () => import('photoswipe'),
+      });
+      lightbox.init();
+
+    };
+    init();
+  }, []);
+  const handleClick = (event: MouseEvent) => {
+    event.preventDefault()
+    const target = event.target as HTMLElement
+    const currentLocation = target.textContent
+    setLocation(currentLocation)
+  }
+
+  return (
+    <>
+      <header class="flex flex-col justify-start items-start mb-12 md:flex-row md:items-end md:justify-between">
+        <div class="flex flex-row gap-2 font-light text-3xl uppercase">
+          <a href="/" class="font-bold">
+            Aitor Blesa
+          </a>
+          <span>Photography</span>
+        </div>
+        <nav class="flex gap-4 tracking-widest uppercase font-semibold text-gray-500">
+          {/* <a href="" class={`transition-colors hover:text-gray-950 ${location === 'Scotland' ? 'text-gray-950' : ''}`} onClick={handleClick}>
+            Scotland
+          </a> */}
+          {/* <a href="" class={`transition-colors hover:text-gray-950 ${location === 'Iceland' ? 'text-gray-950' : ''}`} onClick={handleClick}>
+            Iceland
+          </a> */}
+        </nav>
+      </header>
+      {/* <Gallery location={location} /> */}
+    </>
+  );
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				linkedin: "#0a66c2",
+				instagram: "#f56040",
+				accent: "#4E4CA8",
+			}
+		},
 	},
 	plugins: [],
-}
+};
