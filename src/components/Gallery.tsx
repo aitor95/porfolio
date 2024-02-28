b42137daf5bee31bd702b2d7efab30a5140a8f26
Aitor Blesa <aitor.ble@gmail.com>
2024-02-28 13:25:27 +0100

    added dark mode

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,91 +1,98 @@
-import 'photoswipe/style.css';
-import locationInfo from '@data/meta-gallery-location.json';
-import imagesInfo from '@data/meta-gallery.json';
-import { useEffect, useState } from 'preact/hooks';
-import './styles/Gallery.css';
+import locationInfo from "@data/meta-gallery-location.json";
+import imagesInfo from "@data/meta-gallery.json";
+import "photoswipe/style.css";
+import { useEffect } from "preact/hooks";
+import "./styles/Gallery.css";
 
 interface GalleryProps {
-  location: string
+	location: string;
 }
 
 interface PhotoInfo {
-  height: number;
-  width: number;
+	height: number;
+	width: number;
 }
 
 type Masonry<T> = T & { gap: string; maxcolwidth: string };
 
 declare global {
-  namespace preact.createElement.JSX {
-    interface IntrinsicElements {
-      ['masonry-layout']: Masonry<JSX.HTMLAttributes>;
-    }
-  }
+	namespace preact.createElement.JSX {
+		interface IntrinsicElements {
+			["masonry-layout"]: Masonry<JSX.HTMLAttributes>;
+		}
+	}
 }
 
 export default function Gallery({ location }: GalleryProps) {
+	useEffect(() => {
+		const init = async (): Promise<void> => {
+			await import("@appnest/masonry-layout");
+			const module = await import("photoswipe/lightbox");
+			const PhotoSwipeLightbox = module.default;
+			const lightbox = new PhotoSwipeLightbox({
+				gallery: "#gallery",
+				children: "a",
+				pswpModule: () => import("photoswipe"),
+			});
+			lightbox.init();
+		};
+		init();
+	}, []);
 
-  useEffect(() => {
-    const init = async (): Promise<void> => {
-      await import('@appnest/masonry-layout');
-      const module = await import('photoswipe/lightbox');
-      const PhotoSwipeLightbox = module.default;
-      const lightbox = new PhotoSwipeLightbox({
-        gallery: '#gallery',
-        children: 'a',
-        pswpModule: () => import('photoswipe'),
-      });
-      lightbox.init();
-    };
-    init();
-  }, []);
+	let filteredIndex;
 
+	if (location.trim() === "" || location === "All") {
+		filteredIndex = locationInfo.map((_, index) => index + 1);
+	} else {
+		filteredIndex = locationInfo
+			.map((loc, index) => (loc.location === location ? index + 1 : -1))
+			.filter((index) => index !== -1);
+	}
 
-  let filteredIndex;
+	const filteredImagesInfo = filteredIndex.map(
+		(index) => imagesInfo[index - 1],
+	);
 
-  if (location.trim() === '' || location === "All") {
-    filteredIndex = locationInfo.map((_, index) => index + 1);
-  } else {
-    filteredIndex = locationInfo
-      .map((loc, index) => loc.location === location ? index + 1 : -1)
-      .filter(index => index !== -1);
-  }
+	return (
+		<masonry-layout
+			gap="24"
+			maxcolwidth="500"
+			id="gallery"
+			class="mx-auto w-full"
+		>
+			{filteredIndex.map((index, i) => {
+				const { height, width } = filteredImagesInfo[i];
+	
+				return (
+					<a
+						className="group rounded-md hover:scale-[1.03] transition-transform duration-200"
+						href={`/gallery/${index}.webp`}
+						data-cropped="true"
+						data-pswp-width={width}
+						data-pswp-height={height}
+						target="_blank"
+						rel="noreferrer"
+					>
+						<img
+							className="w-full h-auto rounded-md object-cover"
+							loading="lazy"
+							src={`/gallery/thumbnails/${index}.webp`}
+							width={width}
+							height={height}
+							alt="Imagen de la galería"
 
-  const filteredImagesInfo = filteredIndex.map(index => imagesInfo[index - 1]);
-
-  return (
-    <masonry-layout
-      gap='24'
-      maxcolwidth='500'
-      id='gallery'
-      class="mx-auto w-full"
-    >
-      {filteredIndex.map((index, i) => {
-        const { height, width } = filteredImagesInfo[i]
-        return (
-          <a
-            className="group rounded-md hover:scale-[1.03] transition-transform duration-200"
-            href={`/gallery/${index}.webp`}
-            data-cropped="true"
-            data-pswp-width={width}
-            data-pswp-height={height}
-            target="_blank"
-          >
-            <img
-              className="w-full h-auto rounded-md object-cover"
-              loading="lazy"
-              src={`/gallery/thumbnails/${index}.webp`}
-            />
-            <img
-              className="blur-md opacity-0 group-hover:opacity-100 absolute inset-0 transition contrast-130 -z-10 object-cover"
-              loading="lazy"
-              src={`/gallery/thumbnails/${index}.webp`}
-              alt="Imagen con efecto blur para hacer de sombra de una fotografía"
-            />
-          </a>
-        );
-      })}
-
-    </masonry-layout>
-  );
+						/>
+						<img
+							className="blur-md opacity-0 group-hover:opacity-100 absolute inset-0 transition contrast-130 -z-10 object-cover"
+							loading="lazy"
+							src={`/gallery/thumbnails/${index}.webp`}
+							width={width}
+							height={height}
+							alt="Imagen con efecto blur para hacer de sombra de una fotografía"
+						/>
+					</a>
+				);
+			})}
+		</masonry-layout>
+	);
 }
--- src/components/GalleryBase.tsx
+++ src/components/GalleryBase.tsx
@@ -1,49 +1,51 @@
-import { useState, useEffect } from "preact/hooks";
+import { useState } from "preact/hooks";
+import Locations from "../data/meta-gallery-location.json";
 import Gallery from "./Gallery";
-import Locations from '../data/meta-gallery-location.json';
 
 export default function GalleryBase() {
-  const locationsUnique = [...new Set(Locations.map(loc => loc.location))];
-  const [location, setLocation] = useState<string>('');
+	const locationsUnique = [...new Set(Locations.map((loc) => loc.location))];
+	const [location, setLocation] = useState<string>("");
 
-  const handleClick = (event: MouseEvent) => {
-    event.preventDefault();
-    const target = event.target as HTMLAnchorElement;
-    const currentLocation = target.textContent || '';
-    setLocation(currentLocation);
-  };
+	const handleClick = (event: MouseEvent) => {
+		event.preventDefault();
+		const target = event.target as HTMLAnchorElement;
+		const currentLocation = target.textContent || "";
+		setLocation(currentLocation);
+	};
 
-  return (
-    <>
-      <header class="flex flex-col gap-4 justify-start items-start mb-4 sm:mb-8 lg:mb-12 w-full md:flex-row md:items-end md:justify-between">
-        <div class="flex flex-col gap-2 font-light text-3xl uppercase  ">
-          <a href="/" class="font-bold">
-            Aitor Blesa
-          </a>
-          <span class="text-xl sm:text-2xl">Photography</span>
-        </div>
-        <nav class="flex gap-4 tracking-normal sm:tracking-widest uppercase text-gray-500 text-base sm:text-lg">
-          <a
-            href="#"
-            class={`transition-colors hover:text-gray-950 ${location === 'All' ? 'text-gray-950' : ''}`}
-            onClick={handleClick}
-          >
-            All
-          </a>
-          {
-            locationsUnique.map((locationItem) => (
-
-              <a
-                href="#"
-                class={`transition-colors hover:text-gray-950 ${location === locationItem ? 'text-gray-950' : ''}`}
-                onClick={handleClick}
-              >
-                {locationItem}
-              </a>
-            ))}
-        </nav>
-      </header>
-      <Gallery location={location} />
-    </>
-  );
+	return (
+		<>
+			<header class="flex flex-col gap-4 justify-start items-start mb-4 sm:mb-8 lg:mb-12 w-full md:flex-row md:items-end md:justify-between">
+				<div class="flex flex-col gap-2 font-light text-3xl uppercase  ">
+					<a href="/" class="font-bold text-accent">
+						Aitor Blesa
+					</a>
+					<span class="text-xl sm:text-2xl">Photography</span>
+				</div>
+				<nav class="flex gap-4 tracking-normal sm:tracking-widest uppercase text-content text-base sm:text-lg">
+					<a
+						href="#"
+						class={`transition-colors hover:text-accent ${
+							location === "All" ? "text-accent" : ""
+						}`}
+						onClick={handleClick}
+					>
+						All
+					</a>
+					{locationsUnique.map((locationItem) => (
+						<a
+							href="#"
+							class={`transition-colors hover:text-accent ${
+								location === locationItem ? "text-accent" : ""
+							}`}
+							onClick={handleClick}
+						>
+							{locationItem}
+						</a>
+					))}
+				</nav>
+			</header>
+			<Gallery location={location} />
+		</>
+	);
 }
