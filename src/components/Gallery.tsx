5b4a6b383d987cdebd8bde5519f0c919773f0dd4
Aitor Blesa <aitor.ble@gmail.com>
2024-02-29 12:36:37 +0100

    extract type masonry

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -3,7 +3,7 @@ import imagesInfo from "@data/meta-gallery.json";
 import "photoswipe/style.css";
 import { useEffect } from "preact/hooks";
 import "./styles/Gallery.css";
-
+import { type Masory } from "@components/types/gallery";
 interface GalleryProps {
 	location: string;
 }
@@ -11,16 +11,6 @@ interface GalleryProps {
 interface PhotoInfo {
 	height: number;
 	width: number;
-}
-
-type Masonry<T> = T & { gap: string; maxcolwidth: string };
-
-declare global {
-	namespace preact.createElement.JSX {
-		interface IntrinsicElements {
-			"masonry-layout": Masonry<JSX.HTMLAttributes>;
-		}
-	}
 }
 
 export default function Gallery({ location }: GalleryProps) {
@@ -54,44 +44,46 @@ export default function Gallery({ location }: GalleryProps) {
 	);
 
 	return (
-		<masonry-layout
-			gap="24"
-			maxcolwidth="500"
-			id="gallery"
-			class="mx-auto w-full"
-		>
-			{filteredIndex.map((index, i) => {
-				const { height, width } = filteredImagesInfo[i];
+		<div>
+			<masonry-layout
+				gap="24"
+				maxcolwidth="500"
+				id="gallery"
+				class="mx-auto w-full"
+			>
+				{filteredIndex.map((index, i) => {
+					const { height, width } = filteredImagesInfo[i];
 
-				return (
-					<a
-						className="group rounded-md hover:scale-[1.03] transition-transform duration-200"
-						href={`/gallery/${index}.webp`}
-						data-cropped="true"
-						data-pswp-width={width}
-						data-pswp-height={height}
-						target="_blank"
-						rel="noreferrer"
-					>
-						<img
-							className="w-full h-auto rounded-md object-cover"
-							loading="lazy"
-							src={`/gallery/thumbnails/${index}.webp`}
-							width={width}
-							height={height}
-							alt="Imagen de la galería"
-						/>
-						<img
-							className="blur-md opacity-0 group-hover:opacity-100 absolute inset-0 transition contrast-130 -z-10 object-cover"
-							loading="lazy"
-							src={`/gallery/thumbnails/${index}.webp`}
-							width={width}
-							height={height}
-							alt="Imagen con efecto blur para hacer de sombra de una fotografía"
-						/>
-					</a>
-				);
-			})}
-		</masonry-layout>
+					return (
+						<a
+							className="group rounded-md hover:scale-[1.03] transition-transform duration-200"
+							href={`/gallery/${index}.webp`}
+							data-cropped="true"
+							data-pswp-width={width}
+							data-pswp-height={height}
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								className="w-full h-auto rounded-md object-cover"
+								loading="lazy"
+								src={`/gallery/thumbnails/${index}.webp`}
+								width={width}
+								height={height}
+								alt="Imagen de la galería"
+							/>
+							<img
+								className="blur-md opacity-0 group-hover:opacity-100 absolute inset-0 transition contrast-130 -z-10 object-cover"
+								loading="lazy"
+								src={`/gallery/thumbnails/${index}.webp`}
+								width={width}
+								height={height}
+								alt="Imagen con efecto blur para hacer de sombra de una fotografía"
+							/>
+						</a>
+					);
+				})}
+			</masonry-layout>
+		</div>
 	);
 }
