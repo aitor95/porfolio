f3cafdd1234f28e6bd0fd5d319bcf019c2a9be80
Aitor Blesa <aitor.ble@gmail.com>
2024-06-07 10:13:56 +0200

    fix themetoogle

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -8,10 +8,10 @@ interface GalleryProps {
 	location: string;
 }
 
-interface PhotoInfo {
-	height: number;
-	width: number;
-}
+// interface PhotoInfo {
+// 	height: number;
+// 	width: number;
+// }
 
 export default function Gallery({ location }: GalleryProps) {
 	useEffect(() => {
@@ -44,46 +44,48 @@ export default function Gallery({ location }: GalleryProps) {
 	);
 
 	return (
-		<div>
-			<masonry-layout
-				gap="24"
-				maxcolwidth="500"
-				id="gallery"
-				class="mx-auto w-full"
-			>
-				{filteredIndex.map((index, i) => {
-					const { height, width } = filteredImagesInfo[i];
 
-					return (
-						<a
-							className="group rounded-md hover:scale-[1.03] transition-transform duration-200"
-							href={`/gallery/${index}.webp`}
-							data-cropped="true"
-							data-pswp-width={width}
-							data-pswp-height={height}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<img
-								className="w-full h-auto rounded-md object-cover"
-								loading="lazy"
-								src={`/gallery/thumbnails/${index}.webp`}
-								width={width}
-								height={height}
-								alt="Imagen de la galería"
-							/>
-							<img
-								className="blur-md opacity-0 group-hover:opacity-100 absolute inset-0 transition contrast-130 -z-10 object-cover"
-								loading="lazy"
-								src={`/gallery/thumbnails/${index}.webp`}
-								width={width}
-								height={height}
-								alt="Imagen con efecto blur para hacer de sombra de una fotografía"
-							/>
-						</a>
-					);
-				})}
-			</masonry-layout>
-		</div>
+		// <section id="gallery"
+		// 	class="mx-auto w-full grid gap-6">
+		<masonry-layout
+			gap="24"
+			maxcolwidth="600"
+			class="lg:mx-auto mx-4 py-20"
+			id="gallery"
+		>
+			{filteredIndex.map((index, i) => {
+				const { height, width } = filteredImagesInfo[i];
+				return (
+					<a
+						className="group rounded-md hover:scale-[1.03] transition-transform duration-200 w-full h-auto block"
+						href={`/gallery/${index}.webp`}
+						data-cropped="true"
+						data-pswp-width={width}
+						data-pswp-height={height}
+						target="_blank"
+						rel="noreferrer"
+					>
+						<img
+							className="rounded-md w-full h-auto block"
+							loading="lazy"
+							src={`/gallery/thumbnails/${index}.webp`}
+							width={width}
+							height={height}
+							alt="Imagen de la galería"
+						/>
+						<img
+							className="blur-md opacity-0 group-hover:opacity-100 absolute inset-0 transition contrast-130 -z-10 w-full h-auto block"
+							loading="lazy"
+							src={`/gallery/thumbnails/${index}.webp`}
+							width={width}
+							height={height}
+							alt="Imagen con efecto blur para hacer de sombra de una fotografía"
+						/>
+					</a>
+				)
+			})}
+
+		</masonry-layout>
+
 	);
 }
