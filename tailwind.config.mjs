326dca27f068aecbed37197d533aae71d787f13c
Aitor <aitor.ble@gmail.com>
2025-01-04 14:39:25 +0100

    rewrite pure css to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,9 @@ export default {
 				content: "rgb(var(--color-content) / <alpha-value>)",
 				dropdown: "rgb(var(--color-dropdown))",
 			},
+			boxShadow: {
+				"custom": "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
+			},
 		},
 	},
 	plugins: [],
